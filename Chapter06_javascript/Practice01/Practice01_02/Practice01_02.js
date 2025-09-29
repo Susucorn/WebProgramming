d53f761a6bc6b6aaae6341abf1ef337d3e51491f
Susucorn <127116788+Susucorn@users.noreply.github.com>
2025-09-29 16:30:11 +0900

    Delete Chapter06_javascript directory

--- Chapter06_javascript/Practice01/Practice01_02/Practice01_02.js
+++
@@ -1,7 +0,0 @@
-/* 자바스크립트 작성 */
-function over(obj) {
-    obj.style.background="yellow";
-}
-function out(obj) {
-    obj.style.background="white";       
-}
--- Chapter06_javascript/WriteJavascript/WriteJavascript_03.js
+++
@@ -1,7 +0,0 @@
-// 자바스크립트 작성
-function over(obj) {
-    obj.src="img/AI_커비그림.png";
-}
-function out(obj) {
-    obj.src="img/보라커비.jpg"
-}
